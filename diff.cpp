3cf32c35789640f236cd99df23c082845cb70f0c
abc <yashmeetkaur777@gmail.com>
2022-06-14 13:50:28 +0530

    adding msg in sum.cpp

--- diff.cpp
+++ diff.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main(){
     int a=2;
     int b=3;
-    cout
+    cout<<b-a;
 }
--- sum.cpp
+++ sum.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main(){
     int a=2;
     int b=3;
-    cout<<a+b;
+    cout<<"sum is : "<<a+b;
 }
